8755910a8417bb36d4ddcd87b3d73dd81da51fac
Zeus6768 <akaps6768@gmail.com>
2021-07-03 19:35:50 +0900

    Error Fixed

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-krkncp=&cd_8we=vx1)rh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
